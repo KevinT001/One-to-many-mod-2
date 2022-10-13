0f244254c4cd81c3434726ecfc5d4c3ac9eb8a2e
Kevin Ta <kvnta@yahoo.com>
2022-10-12 19:26:45 -0600

    US 11 complete. Test/Feat: Form to create new parent(lake) form.

--- app/controllers/lakes_controller.rb
+++ app/controllers/lakes_controller.rb
@@ -8,4 +8,17 @@ class LakesController < ApplicationController
     @lake = Lake.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    Lake.create(lake_params)
+    redirect_to "/lakes"
+  end
+
+  private
+
+  def lake_params
+    params.permit(:name, :max_depth, :allows_public_fishing, :lake_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
  
   get '/', to: 'lakes#index'
   get '/lakes', to: 'lakes#index'
+  get '/lakes/new', to: 'lakes#new'
+  post '/lakes', to: 'lakes#create' 
   get '/lakes/:id', to: 'lakes#show'
-
-
-
+  
 
   get '/fishes', to: 'fishes#index'
   get '/fishes/:id', to: 'fishes#show'
   get '/lakes/:id/fishes', to: 'lakes_fishes#index'
+  
 end
