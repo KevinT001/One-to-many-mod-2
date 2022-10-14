802e830e4d4a46afe5953cf79ed40dbfcbad36d7
Kevin Ta <kvnta@yahoo.com>
2022-10-14 15:38:24 -0600

    Add new Child to parent child index page

--- app/controllers/lakes_fishes_controller.rb
+++ app/controllers/lakes_fishes_controller.rb
@@ -4,4 +4,24 @@ class LakesFishesController < ApplicationController
     @lakes = Lake.find(params[:id])
     @fishes = @lakes.fishes
   end
+
+  def new
+    @lake = Lake.find(params[:id])
+  end
+  
+  def create 
+
+    @lake = Lake.find(params[:id])
+    @fish = @lake.fishes.create!(lake_fish_params)
+    # binding.pry
+
+    # fish = LakeFish.create!(lake_id:params[:fish], fish_id: params[:id])
+    # Lake.find(params[:id])
+    redirect_to "/lakes/#{@lake.id}/fishes"
+  end
+
+private
+  def lake_fish_params
+    params.permit(:name, :avg_length, :catch_and_release_only, :lake_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 
+  
   get '/', to: 'lakes#index'
   get '/lakes', to: 'lakes#index'
   get '/lakes/new', to: 'lakes#new'
@@ -8,9 +8,13 @@ Rails.application.routes.draw do
   patch '/lakes/:id', to: 'lakes#update'
   get '/lakes/:id', to: 'lakes#show'
   
-
+  
+  get '/lakes/:id/fishes/new', to: 'lakes_fishes#new'
+  get '/lakes/:id/fishes', to: 'lakes_fishes#index'
+  post 'lakes/:id/fishes', to: 'lakes_fishes#create'
+  
+  
   get '/fishes', to: 'fishes#index'
   get '/fishes/:id', to: 'fishes#show'
-  get '/lakes/:id/fishes', to: 'lakes_fishes#index'
-  
+
 end
